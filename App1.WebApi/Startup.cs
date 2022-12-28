edcf1bf7f03e8e4d0878f35f38fd30226868c296
omerace <omerace@gmail.com>
2022-12-29 00:52:40 +0300

    Add project files.

---
+++ App1.WebApi/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using App1.WebApi.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+
+namespace App1.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpClient();
+
+            services.AddOpenTelemetryTracing(builder =>
+            {
+                builder.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddSource(nameof(PublishMessageController))
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("App1"))
+                    .AddOtlpExporter(opts =>
+                    {
+                        opts.Endpoint = new Uri(Configuration["Otlp:Endpoint"]);
+                        opts.ExportProcessorType = ExportProcessorType.Simple;
+                    });
+            });
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddOpenTelemetryMetrics(builder => builder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("App1"))
+                .AddMeter("App1-Metrics")
+                .AddHttpClientInstrumentation()
+                .AddRuntimeInstrumentation()
+                .AddOtlpExporter(opts =>
+                {
+                    opts.Endpoint = new Uri(Configuration["Otlp:Endpoint"]);
+                })
+                );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/health", async context =>
+                {
+                    await context.Response.WriteAsync("Ok");
+                });
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ App3.WebApi/Controllers/MessageToSqlController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using App3.WebApi.Events;
+using App3.WebApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+
+namespace App3.WebApi.Controllers
+{
+    [ApiController]
+    [Route("sql-to-event")]
+    public class MessageToSqlController : ControllerBase
+    {
+        private readonly ISqlRepository _repository;
+        private readonly IRabbitRepository _eventPublisher;
+        private readonly ILogger<MessageToSqlController> _logger;
+
+        public MessageToSqlController(ISqlRepository repository, 
+            IRabbitRepository eventPublisher, 
+            ILogger<MessageToSqlController> logger)
+        {
+            _repository = repository;
+            _eventPublisher = eventPublisher;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task PostMessage([FromBody]string message)
+        {
+            var bg1 = Baggage.GetBaggage("App1");
+            var bg2 = Baggage.GetBaggage("App2");
+            Baggage.SetBaggage("App3", "PostMessage");
+            _logger.LogTrace("You call sql save message endpoint");
+           if (!string.IsNullOrEmpty(message))
+           {
+               await _repository.Persist(message);
+               _eventPublisher.Publish(new MessagePersistedEvent {Message = message});
+           }
+
+        }
+    }
+}
